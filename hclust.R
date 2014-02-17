4b8a5dbeb04446a32a8df78c0cc742a292d4b275
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-16 19:21:33 -0500

    cleanup of whitespace

--- hclust.R
+++ hclust.R
@@ -23,24 +23,17 @@ dtm <- create_matrix(cbind(as.vector(corpus)), language="english", minDocFreq=1,
               weighting=weightTf)
 rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
 dtm   <- dtm[rowTotals> 0]           #remove all docs without words
-
 dtm <- removeSparseTerms(dtm, .95)
 plot(hclust(dist(dtm), method="complete"), xlab="text from corpus", "ylab"="distance", main="Cluster Dendrogram of Various Texts")
-
 op = par(bg="#DDE3CA")
-
 plot(dtm_complete, col="#487AA1", col.main="#45ADA8", col.lab="#7C8071",
      col.axis="#F38630", lwd=1, lty=1, sub='', hang=-1, axes=FALSE,
      main = "Cluster Dendrogram Representing Author Similarity", 
      xlab="Author/Publication", ylab = "Stylistic Distance")
-
 par(op)
-
 nplot(dtm_complete, hang=1, axes = TRUE, ann=TRUE, main = "Cluster Dendrogram Representing Author Similarity", 
       xlab="Author/Publication", ylab = "Distance")
-
 phyl <- as.phylo(hclust(dtm_distro))
-
 plot(phyl, edge.col=c("blue", "green", "red")[c(TRUE, FALSE) + 1 + (phyl$edge.length > 20)])
 
 
